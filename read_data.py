84122dea820259b01a121f3b6f124a5d73f6e386
rathodabhi0 <rathodabhi0@gmail.com>
2020-03-07 15:53:54 -0500

    reading the data

--- read_data.py
+++ read_data.py
@@ -2,3 +2,17 @@ import os
 import pandas as pd
 import numpy as np
 
+class reading_data:
+
+    def __init__(self, file_name):
+        self.file_name=file_name
+
+    def import_file(self):
+        df = pd.read_csv(self.file_name)
+
+        print(df.head())
+
+
+p = reading_data('LS_20.csv')
+
+p.import_file()
