86712d64477bbdb77eadd5894b152ff627756ee4
rathodabhi0 <rathodabhi0@gmail.com>
2020-03-08 18:11:55 -0400

    Grouping party wise seats for every state

--- read_data.py
+++ read_data.py
@@ -9,10 +9,18 @@ class reading_data:
 
     def import_file(self):
         df = pd.read_csv(self.file_name)
+        return df
 
-        print(df.head())
 
+class statewide_analysis:
 
-p = reading_data('LS_20.csv')
+    def seats(self):
+        place_holder_for_df = reading_data('LS_20.csv')
+        df1 = place_holder_for_df.import_file()
+        print(df1.groupby(['STATE','PARTY']).sum())
 
-p.import_file()
+if __name__ == "__main__":
+
+    p = statewide_analysis()
+    p.seats()
+
