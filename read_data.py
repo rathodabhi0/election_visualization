26c4d44a3236bb809b736f03ce4607ced17c5a17
abhi <rathodabhi0@gmail.com>
2020-03-24 22:21:18 -0400

    Candidate count per party

--- read_data.py
+++ read_data.py
@@ -21,22 +21,37 @@ class statewide_analysis:
         self.df1=df1
 
     def state_wise_party_vote_share(self):
+        print("Party vote share in state")
         df2 = df1[['STATE','PARTY','TOTAL VOTES', 'TOTAL ELECTORS']]
         df2['Vote share']= ((df2['TOTAL VOTES']/df2['TOTAL ELECTORS'])*100)
         return df2[['STATE', 'PARTY', 'Vote share']].groupby(['STATE', 'PARTY']).sum()
 
-    def state_constituency(self):
-        return df1.groupby(['STATE','CONSTITUENCY', 'TOTAL ELECTORS']).sum()
+    def state_constituency_party_winner(self):
+        print("Constituency winner by Party")
+        df2 = df1.loc[df1.WINNER == 1]
+        return df2[['STATE', 'CONSTITUENCY', 'PARTY', 'WINNER']]
 
     def gender(self):
-        return df1.groupby(['STATE', 'GENDER', 'TOTAL ELECTORS']).sum()
+        print("This is gender table")
+        df2 = df1[['STATE', 'GENDER']]
+        return df2.groupby(['STATE', 'GENDER']).sum()
 
 class partywide_analysis:
     def __init__(self, df1):
         self.df1=df1
 
     def party_vote(self):
-        return df1.groupby(['PARTY']).sum()
+        print("Party vote share in Nationally")
+        df2 = df1[['PARTY','TOTAL VOTES']].groupby(['PARTY']).sum()
+        df3 = df1[['TOTAL VOTES'][0]].sum()
+        df2['Vote share'] = ((df2['TOTAL VOTES']/df3)*100)
+        return df2.sort_values(by=['Vote share'], ascending=False)
+
+    def total_candidates_by_party(self):
+        print("Total number of candidates for each party")
+        df2 = df1[df1.PARTY != 'NOTA']
+        df2 = df2['PARTY'].value_counts()
+        return
 
 if __name__ == "__main__":
     abhi= 'LS_20.csv'
@@ -45,6 +60,7 @@ if __name__ == "__main__":
     statewide_analysis_method = statewide_analysis(df1)
     partywide_analysis_method= partywide_analysis(df1)
     print(statewide_analysis_method.state_wise_party_vote_share())
-    print(statewide_analysis_method.state_constituency())
+    print(statewide_analysis_method.state_constituency_party_winner())
     print(statewide_analysis_method.gender())
     print(partywide_analysis_method.party_vote())
+    print(partywide_analysis_method.total_candidates_by_party())
