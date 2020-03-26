57eb36e6e9021bd43739c2589ec8aea121626b14
abhi <rathodabhi0@gmail.com>
2020-03-25 23:47:49 -0400

    Properly formatted output

--- read_data.py
+++ read_data.py
@@ -2,9 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 import redis
-# import dash
-# import dash_core_components as dcc
-# import dash_html_components as html
+
 
 class reading_data:
 
@@ -24,17 +22,20 @@ class statewide_analysis:
         print("Party vote share in state")
         df2 = df1[['STATE','PARTY','TOTAL VOTES', 'TOTAL ELECTORS']]
         df2['Vote share']= ((df2['TOTAL VOTES']/df2['TOTAL ELECTORS'])*100)
-        return df2[['STATE', 'PARTY', 'Vote share']].groupby(['STATE', 'PARTY']).sum()
-
+        return df2.groupby(['STATE'], as_index=False)['PARTY', 'Vote share'].agg(lambda x: list(x))
+#        df3.to_csv('export.csv', index=False)
     def state_constituency_party_winner(self):
         print("Constituency winner by Party")
         df2 = df1.loc[df1.WINNER == 1]
-        return df2[['STATE', 'CONSTITUENCY', 'PARTY', 'WINNER']]
+        return df2[['STATE', 'CONSTITUENCY', 'PARTY']]
 
     def gender(self):
         print("This is gender table")
-        df2 = df1[['STATE', 'GENDER']]
-        return df2.groupby(['STATE', 'GENDER']).sum()
+        df2 = df1.dropna()
+        df4 = df2.groupby(['STATE', 'GENDER'],as_index=False).sum()
+        return df4[['STATE', 'GENDER', 'WINNER']]
+
+#        df3.to_csv('export.csv', index=False)
 
 class partywide_analysis:
     def __init__(self, df1):
@@ -42,16 +43,32 @@ class partywide_analysis:
 
     def party_vote(self):
         print("Party vote share in Nationally")
-        df2 = df1[['PARTY','TOTAL VOTES']].groupby(['PARTY']).sum()
+        df2 = df1[['PARTY','TOTAL VOTES']].groupby(['PARTY'], as_index=False).sum()
         df3 = df1[['TOTAL VOTES'][0]].sum()
         df2['Vote share'] = ((df2['TOTAL VOTES']/df3)*100)
         return df2.sort_values(by=['Vote share'], ascending=False)
+
 
     def total_candidates_by_party(self):
         print("Total number of candidates for each party")
         df2 = df1[df1.PARTY != 'NOTA']
         df2 = df2['PARTY'].value_counts()
-        return
+        return df2
+
+
+class candidate_analysis:
+    def __init__(self, df1):
+        self.df1=df1
+
+    def criminal_case_candidate(self):
+        df2 = df1.dropna()
+        df3 = df2[['NAME','CRIMINAL CASES']]
+        df3.replace(to_replace=['0','Not Available'], value=np.NaN, inplace = True)
+        df5 = df3.dropna()
+        df5['CRIMINAL CASES'] = df5['CRIMINAL CASES'].astype(int)
+        df7 = df5.sort_values(by=['CRIMINAL CASES'], ascending=True)
+        return df7
+
 
 if __name__ == "__main__":
     abhi= 'LS_20.csv'
@@ -59,8 +76,10 @@ if __name__ == "__main__":
     df1 = place_holder_for_df.import_file()
     statewide_analysis_method = statewide_analysis(df1)
     partywide_analysis_method= partywide_analysis(df1)
+    candidate_analysis_method= candidate_analysis(df1)
     print(statewide_analysis_method.state_wise_party_vote_share())
     print(statewide_analysis_method.state_constituency_party_winner())
     print(statewide_analysis_method.gender())
     print(partywide_analysis_method.party_vote())
     print(partywide_analysis_method.total_candidates_by_party())
+    print(candidate_analysis_method.criminal_case_candidate())
